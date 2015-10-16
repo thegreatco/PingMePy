385703cd51aa304153f1a3e609da9f551a6fffc4
Peter Garafano <thegreatco@thegreatco.com>
2015-10-16 16:21:42 -0400

    Added files to support publishing to PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name='PingMePy',
+  packages=['PingMePy'],
+  version='0.1',
+  description='A wrapper library around the MongoDB OpsManager / Cloud Manager API',
+  author='TheGreatCO',
+  author_email='thegreatco@thegreatco.com',
+  url='https://github.com/peterldowns/mypackage',
+  download_url='https://github.com/thegreatco/PingMePy/tarball/0.1',
+  keywords=['mms', 'cloud', 'opsmanager', 'cloudmanager', 'mongodb'],
+  classifiers=[], requires=['requests']
+)
+__author__ = 'TheGreatCO'
