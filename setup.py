edc3c09e4a3933f1c9310a59e00546128f240ffd
TheGreatCO <thegreatco@thegreatco.com>
2016-12-30 16:43:41 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='PingMePy',
     packages=['PingMePy'],
-    version='0.4',
+    version='0.5',
     description='A wrapper library around the MongoDB OpsManager / Cloud Manager API',
     author='TheGreatCO',
     author_email='thegreatco@thegreatco.com',
